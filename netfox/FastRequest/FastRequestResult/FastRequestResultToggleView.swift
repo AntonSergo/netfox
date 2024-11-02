a7de9f7bb0563e813e049669adc03235632d0b24
user1000 <user1000@unlabeled.qupra.nl>
2024-11-02 15:35:54 +0100

    added fix

--- netfox/FastRequest/FastRequestResult/FastRequestResultToggleView.swift
+++ netfox/FastRequest/FastRequestResult/FastRequestResultToggleView.swift
@@ -55,8 +55,6 @@ struct SymbolToggleStyle: ToggleStyle {
                         .padding(3)
                         .overlay {
                             Image(systemName: systemImage)
-                                .resizable()
-                                .frame(width: 15, height: 18)
                                 .foregroundColor(configuration.isOn ? .gray : .white)
                         }
                         .offset(x: configuration.isOn ? 10 : -10)
