884f2fcde7b34e78787dd55b58a3e229cfc23564
user1000 <user1000@unlabeled.qupra.nl>
2024-11-05 15:35:20 +0100

    added fix

--- netfox/FastRequest/FastRequestFirst/FastRequest1View.swift
+++ netfox/FastRequest/FastRequestFirst/FastRequest1View.swift
@@ -101,6 +101,7 @@ public struct FastRequest1View: View {
                     }
                     .scrollIndicators(.hidden)
                     .frame(minHeight: 50)
+                    .padding(.top)
                     
                     Button(action: {
                         
